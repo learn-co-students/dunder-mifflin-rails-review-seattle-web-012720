7056a60e2bd22abe55a4e8f1fa96a610a4b9e82c
kailanak1 <kailanakahawaii@gmail.com>
2020-02-27 12:55:16 -0800

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,38 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all 
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        @employees = Employee.all
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit 
+        @dog = Dog.find(params[:id])
+
+    end
+
+    def update
+        @dog = Dog.find(params[:id])
+
+    end
+
+    private 
+    def dog_params 
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
+    def employee_params 
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all 
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new 
+    end
+
+    def create
+     
+        @employee = Employee.new(employee_params) 
+         if @employee.valid?
+             @employee.save
+            redirect_to @employee 
+         else 
+            flash[:messages] = @employee.errors.full_messages
+            render :new
+         end
+
+    end
+
+    def edit 
+        @employee = Employee.find(params[:id])
+
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        
+        if @employee.save
+            redirect_to employees_path 
+        else 
+            flash[:messages] = @employee.errors.full_messages 
+            render :edit
+        end
+            
+    end
+
+    private 
+    def employee_params 
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
 end
