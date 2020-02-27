d4ff6c3c5a4fc9cccad54eed10d3a86f2cb4d327
clheard54 <clarion.heard@gmail.com>
2020-02-27 09:53:22 -0800

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,11 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def self.sorted(sort)
+        if sort
+            self.all.sort_by {|dog| dog.name}
+        else
+            Dog.all
+        end
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,17 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :title, uniqueness: true
+    validates :alias, uniqueness: true
+
+    def self.search(search)
+        if search
+          dog = Dog.find_by(name: search)
+          if dog
+            self.where(dog_id: dog)
+          else Employee.all
+          end
+        else
+          Employee.all
+        end
+    end
 end
