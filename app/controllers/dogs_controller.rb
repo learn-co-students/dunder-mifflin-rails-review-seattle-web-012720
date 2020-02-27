59cd0b73b1e60e276ee926d1968a2e0250c51db1
javila35 <josepharthuravila@gmail.com>
2020-02-27 10:47:52 -0800

    finished project

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,41 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.sorted(params[:sort])
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def update
+        @dog = Dog.create(dog_params)
+        if @dog.valid?
+            redirect_to @dog
+        else 
+            render :new
+        end
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+    end
+    
+    def update
+        @dog = Dog.find(params[:id])
+        @dog.update
+        if @dog.valid?
+            redirect_to @dog
+        else
+            render :new
+        end
+    end
+
+    private
+        def dog_params
+            params.require(:dog).permit(:name, :breed, :age, :sort)
+        end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,45 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        @dog = Dog.find(@employee.dog_id)
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        if @employee.valid?
+            redirect_to @employee
+        else 
+            flash[:notice] = @employee.errors.full_messages
+            render :edit
+        end
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        if @employee.valid?
+            redirect_to @employee
+        else
+            flash[:notice] = @employee.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+        def employee_params
+            params.require(:employee).permit(:first_name,:last_name,:alias, :title, :office, :img_url, :dog_id)
+        end
 end
