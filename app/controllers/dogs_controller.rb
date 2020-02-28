a97ea603efda2092450184dbce8dff7923746ffa
Agandaur <cjwagstaffe@gmail.com>
2020-02-28 08:36:04 -0800

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,45 @@
 class DogsController < ApplicationController
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create 
+        @dog = Dog.create(d_params)
+            redirect_to @dog
+    end
+
+    def show 
+        @dog = Dog.find(params[:id])
+    end
+    
+    def index 
+        @dogs = Dog.all
+    end
+
+    def edit 
+        @dog = Dog.find(params[:id])
+    end
+
+    def update 
+        @dog = Dog.find(params[:id])
+        @dog.update(d_params)
+        redirect_to @dog
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect_to dogs_path
+    end 
+
+    private 
+
+    def d_params
+        params.require(:dog).permit(
+            :name, 
+            :breed,  
+            :age
+        )
+    end
 end
